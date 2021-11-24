ddb316492b19b532896e34eac94bbe59dc2259cc
Kathryn Grose <kgrose@csumb.edu>
2021-11-23 21:18:34 -0800

    compatible with android connection

--- test_node_server/app.js
+++ test_node_server/app.js
@@ -1,6 +1,58 @@
 const http = require("http");
+/*
+const { initializeApp } = require('firebase-admin/app');
+const { getFirestore, collection, getDocs } = require('firebase/firestore');
+*/
 const hostname = '127.0.0.1';
 const port = 3000;
+
+/*
+const db = getFirestore();
+const ref = db.ref();
+const usersRef = ref.child('Users');
+const app = initializeApp();
+*/
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("C:/Users/Kat/Desktop/Node/myplants-438p3g11-firebase-adminsdk-sgpue-27cf720786.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://myplants-438p3g11-default-rtdb.firebaseio.com/',
+    projectId: 'myplants-438p3g11'
+});
+
+// add new user
+
+// send user
+
+// test user
+
+
+
+/*
+usersRef.set({
+    testuser2: {
+        password: testpass2
+    }
+});
+*/
+
+// login
+
+// logout
+
+// delete user
+
+// add plant to private list
+
+// add plant to public list
+
+// delete plant from private list
+
+// delete plant from public list
+
 
 //Create HTTP server and listen on port 3000 for requests
 const server = http.createServer((req, res) => {
@@ -8,10 +60,10 @@ const server = http.createServer((req, res) => {
     //Set the response HTTP header with HTTP status and Content type
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('New Test Node Server\n');
+    res.end('Connected to myPlants Node Server\n');
 });
 
 //listen for request on port 3000, and as a callback function have the port listened on logged
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server myPlants running at http://${hostname}:${port}/`);
 });
