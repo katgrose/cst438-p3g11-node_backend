a5b4cb08aaf645ea08e9b8b604ae97133c06c013
Kathryn Grose <kgrose@csumb.edu>
2021-11-29 15:45:51 -0800

    added api routing and all database collections

--- test_node_server/app.js
+++ test_node_server/app.js
@@ -1,10 +1,9 @@
 const http = require("http");
+var admin = require("firebase-admin");
+var serviceAccount = require("./serviceAccountKeys.json");
+
 const hostname = '127.0.0.1';
 const port = 3000;
-
-var admin = require("firebase-admin");
-
-var serviceAccount = require("./serviceAccountKeys.json");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -12,14 +11,36 @@ admin.initializeApp({
     projectId: 'myplants-438p3g11'
 });
 
+// database collection paths
 const db = admin.firestore();
 let userRef = db.collection("User");
+let publicPlantRef = db.collection("public_plants")
+let userPlantRef = db.collection("user_plants")
 
-userRef.get().then((querySnapshot) => {
-    querySnapshot.forEach(document => {
-        console.log(document.data())
-    })
-})
+// test get users
+// userRef.get().then((querySnapshot) => {
+//     querySnapshot.forEach(document => {
+//         console.log(document.data())
+//     })
+// })
+//
+// // test get public plants using ejs
+// publicPlantRef.get().then((querySnapshot) => {
+//     querySnapshot.forEach(document => {
+//         console.log(document.data())
+//     })
+// })
+//
+// // test get user plants using ejs
+// userPlantRef.get().then((querySnapshot) => {
+//     querySnapshot.forEach(document => {
+//         console.log(document.data())
+//     })
+// })
+//
+// userRef.doc('testuser1').get().then((querySnapshot) => {
+//         console.log(querySnapshot.data())
+// })
 
 // add new user
 
@@ -45,10 +66,20 @@ userRef.get().then((querySnapshot) => {
 //Create HTTP server and listen on port 3000 for requests
 const server = http.createServer((req, res) => {
 
-    //Set the response HTTP header with HTTP status and Content type
-    res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Connected to myPlants Node Server\n');
+    if(req.url == '/getUsers') {
+        userRef.doc('testuser1').get().then((querySnapshot) => {
+            res.statusCode = 200;
+            res.write(querySnapshot.data().username + " ");
+            res.write(querySnapshot.data().password)
+            res.end();
+        })
+    };
+
+    //Set the response HTTP header with HTTP status and Content type
+
+    // res.write('stuff');
+    // res.end('End of line');
 });
 
 //listen for request on port 3000, and as a callback function have the port listened on logged
